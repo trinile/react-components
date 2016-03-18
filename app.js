72722bf069eea855aa7877308317876e9ad4a79f
Trini Le <trinile@gmail.com>
2016-03-18 11:37:49 -0700

    Refactored GroceryListItem component to take in groceryItems object

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+// // // TODO
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryListItem groceryItems={['cucumber', 'kale', 'tomatoe']}/>
+    <Cucumber/>
+    <Kale/>
+  </div>
+);
+var GroceryListItem = (props) => {
+
+  return (
+    <ul> 
+      { props.groceryItems.map((item) => {
+        return <li> { item } is part of GroceryListItem Component</li>
+      }) }
+    </ul>
+  )
+};
+
+var Cucumber = () => (
+  <li>Cucumber is a component</li>
+);
+
+var Kale = () => (
+  <li>Kale is  component</li>
+);
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
